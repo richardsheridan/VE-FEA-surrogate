a6904c8011922735d39fd8f2432b8a87e9ffaff0
anqiclaire <anqi.claire.lin@gmail.com>
2023-01-15 00:14:25 -0500

    add dataset for image inputs required by CNN

--- simple_ANN/utils.py
+++ simple_ANN/utils.py
@@ -25,6 +25,42 @@ class VEDataset(Dataset):
     def __len__(self):
         return self.len
 
+class VECNNDataset(Dataset):
+    def __init__(
+        self,
+        json_file,
+        image_col='intph_img',
+        input_idx=[1,37],
+        output_idx=[37,67]):
+        '''
+        param json_file: path to the json dump of a pandas dataframe
+        type json_file: str
+        
+        param image_col: the column name of the column that contains microstructure image file name in dataframe
+        type image_col: str
+
+        param input_idx: the column index range where the input parameters start and end.
+        type index_idx: List(Int)
+
+        param output_idx: the column index range where the output parameters start and end.
+        tpye output_idx: List(Int)
+        '''
+        self.df = pd.read_json(json_file)
+        self.len = len(self.df)
+        self.image_col = image_col
+        self.input_idx = input_idx
+        self.output_idx = output_idx
+        
+    def __getitem__(self, index):
+        item = {}
+        item['image'] = torch.as_tensor(np.load(self.df.iloc[index][self.image_col])[np.newaxis,...],dtype=torch.float)
+        item['input'] = torch.as_tensor(self.df.iloc[index][self.input_idx[0]:self.input_idx[1]].values,dtype=torch.float)
+        item['output'] = torch.as_tensor(self.df.iloc[index][self.output_idx[0]:self.output_idx[1]].values,dtype=torch.float)
+        return item
+
+    def __len__(self):
+        return self.len
+
 # mean absolute percentage error used by Yixing
 # MAPE = 1/N * sum_N{|(y_pred-y_true)/y_true|*100%}
 def MAPELoss(output, target):
